910809f232c53f9727b87f2bb112fb11b2182798
Lorenzo <templarec@gmail.com>
2021-05-10 16:59:36 +0200

    Esercizio completato

--- fetch.php
+++ fetch.php
@@ -1,7 +1,7 @@
 <?php
 require 'db.php';
 header('Content-Type: application/json');
-
+// se l'array non + vuoto e non c'è la variabile stanza fai la lista di tutte le stanze
 if(!empty($_GET) && empty($_GET['stanza'])) {
     $query = "select * from stanze";
     $result = $conn->query($query);
@@ -20,7 +20,7 @@ if(!empty($_GET) && empty($_GET['stanza'])) {
 
 
     $conn->close();
-
+//se la stanza è specificata , prendi la singola stanza
 } elseif (!empty($_GET['stanza'])){
     $stanza = $_GET['stanza'];
     $query = "select * from stanze where id='$stanza'";
--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 var app = new Vue({
     el: '#root',
-    data: {},
+    data: {
+        stanze:{},
+        currentStanza: {},
+        result: false,
+        currentSelection: 0
+    },
+    mounted () {
+        axios.get('./fetch.php', {
+            params: {
+                fetch: "fetch"
+            }
+        }).then((risposta)=>{
+            this.stanze = risposta.data.response
+        })
+    },
     computed: {},
-    methods: {}
+    methods: {
+        getInfo: function (id) {
+            this.currentSelection = id
+            axios.get('./fetch.php', {
+                params: {
+                    stanza: id
+                }
+            }).then((risposta)=>{
+                this.currentStanza = risposta.data.response[0]
+            })
+        }
+    }
 });
